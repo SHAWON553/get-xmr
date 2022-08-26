8b854f182f06fa12040bf8219b57c314f667e262
SHAWON553 <104970948+SHAWON553@users.noreply.github.com>
2022-08-26 11:02:22 +0600

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -6,8 +6,7 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
- 
-wget https://github.com/xmrig/xmrig-nvidia/archive/refs/tags/v2.14.5.tar.gz
-tar xvzf xmrig-nvidia-2.14.5.tar.gz
-cd xmrig-nvidia-2.14.5
-./xmrig --donate-level 5 --cuda -o pool.supportxmr.com:443 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P -k --tls -p simon
+ wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvzf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig --donate-level 5 -o pool.supportxmr.com:443 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P -k --tls -p simon
